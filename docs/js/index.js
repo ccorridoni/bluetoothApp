51deca4ab1ef0594f2f1f61643f1f213c46ec2c3
Chris Corridoni <ccorridoni@OneSkys-MBP.home>
2025-07-21 16:57:41 -0400

    Removed call to unknown element

--- docs/js/index.js
+++ docs/js/index.js
@@ -23,7 +23,7 @@ document.addEventListener('deviceready', onDeviceReady, false);
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+    //document.getElementById('deviceready').classList.add('ready');
 
     // **FIX APPLIED HERE**
     // We now use the ble object to check for permissions and start the scan.
